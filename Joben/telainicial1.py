8d03ef046bd14a9741b2801952a6d41da7b18936
Matheus Chaves <88739142+Matheuschxves@users.noreply.github.com>
2021-11-08 20:46:59 -0300

    Update telainicial1.py

--- Joben/telainicial1.py
+++ Joben/telainicial1.py
@@ -3,7 +3,7 @@ print("Olá Seja Bem vinda ao Joben")
 a = int(input(''' Deseja ir em busca de alguma oportunidade de emprego? 
 [1]- Sim, Me mostre as oportunidades de emprego disponível
 [2]- Não, Desejo continuar na tela inicial.
-'''))
+Resposta: '''))
 time.sleep(1)
 sim = 1
 não = 2
@@ -47,7 +47,7 @@ if a == 1:
     [2]- Coca-Cola
     [3]- MGM Tecnologia
     [4]- Netflix
-    '''))
+    Resposta: '''))
     time.sleep(1)
     esposende = 1
     cocacola = 2
@@ -58,60 +58,61 @@ if a == 1:
         contato1 = int(input('''Você deseja entrar em contato com essa empresa?
         [1]-Sim.
         [2]-Não.
-        '''))
+        Resposta: '''))
         time.sleep(1)
         sim = 1 
         nao = 2
         if contato1 == 1:
-            int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você")) 
+            int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você: ")) 
             time.sleep(1)
             print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.") 
         if contato1 == 2:
             print("Okay, levaremos você de volta a tela inicial!")
-        if empresa  == 2:
-            print("Você selecionou a empresa Coca-Cola")
+    if empresa  == 2:
+        print("Você selecionou a empresa Coca-Cola")
+        time.sleep(1)
+        contato2 = int(input('''Você deseja entrar em contato com essa empresa?
+        [1]-Sim.
+        [2]-Não.
+        Resposta: '''))
+        time.sleep(1)
+        sim = 1 
+        não = 2
+        if contato2 == 1:
+            int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você: ")) 
             time.sleep(1)
-            contato2 = int(input('''Você deseja entrar em contato com essa empresa?
-            [1]-Sim.
-            [2]-Não.
-            '''))
+            print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.")
+        if contato2 == 2:
+            print("Okay, levaremos você de volta a tela inicial!")
+    if empresa == 3:
+        print("Você selecionou a empresa MGM Tecnologia")
+        contato3 = int(input('''Você deseja entrar em contato com essa empresa?
+        [1]-Sim.
+        [2]-Não.
+        Resposta: '''))
+        time.sleep(1)
+        sim = 1
+        não = 2
+        if contato3 == 1:
+            int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você: ")) 
             time.sleep(1)
-            sim = 1 
-            não = 2
-            if contato2 == 1:
-                int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você")) 
-                time.sleep(1)
-                print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.")
-            if contato2 == 2:
-                print("Okay, levaremos você de volta a tela inicial!")
-            if empresa == 3:
-                print("Você selecionou a empresa MGM Tecnologia")
-                contato3 = int(input('''Você deseja entrar em contato com essa empresa?
-                [1]-Sim.
-                [2]-Não.
-                '''))
-                time.sleep(1)
-                sim = 1
-                não = 2
-                if contato3 == 1:
-                    int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você")) 
-                    time.sleep(1)
-                    print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.")
-                if contato3 == 2:
-                    print("Okay, levaremos você de volta a tela inicial!")
-            if empresa == 4:
-                print("Você selecionou a empresa Netflix")
-                contato4 = int(input('''Você deseja entrar em contato com essa empresa?
-                [1]-Sim.
-                [2]-Não.
-                '''))
-                time.sleep(1)
-                sim = 1
-                não = 2
-                if contato4 == 1:
-                    int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você")) 
-                    time.sleep(1)
-                    print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.")
-                if contato4 == 2:
-                    print("Okay, levaremos você de volta a tela inicial!")
-            
+            print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.")
+        if contato3 == 2:
+            print("Okay, levaremos você de volta a tela inicial!")
+    if empresa == 4:
+        print("Você selecionou a empresa Netflix")
+        contato4 = int(input('''Você deseja entrar em contato com essa empresa?
+        [1]-Sim.
+        [2]-Não.
+        Resposta: '''))
+        time.sleep(1)
+        sim = 1
+        não = 2
+        if contato4 == 1:
+            int(input("Seja bem vindo ao nosso chat, Informe um número de whatsapp para entrarmos em contato com você: ")) 
+            time.sleep(1)
+            print("Seu número foi confirmado, entrameros em contato com você para acertamos direto essa contratação.")
+        if contato4 == 2:
+            print("Okay, levaremos você de volta a tela inicial!")
+if a == 2:
+    print("Você continuará na tela inicial!")
